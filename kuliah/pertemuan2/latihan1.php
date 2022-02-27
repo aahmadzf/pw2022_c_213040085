120b27b09fd1915be21ccca9e48e798ddae3eca5
aahmadzf <faturrahmannn08@gmail.com>
2022-02-27 18:50:14 +0700

    mengerjakan latihan pertemuan 2

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,51 @@
+<?php echo 10;
+
+// Komentar
+
+// variable
+// integer angka
+// string tulisan
+// boolean true dan false, jika true = 1 false = null(tidak ada nilai)
+// nama bebas tidak boleh ada spasi
+// ada angka tapi tidak diawal
+// 
+$nama = "ahmad";
+echo $nama;
+
+echo "<hr>";
+
+
+// oprator
+// oprator aritmatika
+// +, -, *, /, %
+//  % modulus (sisa pembagian)
+
+echo (1 + 2) * 3 - 4;
+echo "<br>";
+
+echo 10 % 5;
+
+echo "<hr>";
+
+// oprator perbandingan
+// <, >, =,
+
+echo 2 < 4;
+
+echo "<br>";
+
+// indentity
+// ===, /==
+// untuk mengecek nilai serta tipe data
+
+echo 10 === 10;
+
+echo "<hr>";
+
+// interment dan determent
+// penambahan dan pengurangan 1
+// ++, --
+
+$X = 20;
+$X++;
+echo $X;
---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,27 @@
+<?php
+
+// pengkondisian ? percabangan
+// if else
+// if else if else
+// tenary
+// switch
+
+
+// if else
+// $x = 50;
+// if ($x < 20) {
+//     echo "benar";
+// } else {
+//     echo "salah";
+// }
+
+
+// else if
+// $x = 5;
+// if ($x < 20) {
+//     echo "benar";
+// } else if ($x == 20) {
+//     echo "benar sekali!";
+// } else {
+//     echo "salah";
+// }
